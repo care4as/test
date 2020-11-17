7c9e71a14a5a0c7538a845fddb8d558949068f9d
AndreasRobrahn <andreasrobrahn@care4as.de>
2020-11-17 15:09:36 +0100

    user table angepasst

--- care4as/database/migrations/2014_10_12_000000_create_users_table.php
+++ care4as/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->nullable();
+            $table->integer('person_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
