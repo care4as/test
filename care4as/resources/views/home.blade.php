ee11a5a8997c302dece16ccd6aee74c60c94f33d
AndreasRobrahn <andreasrobrahn@care4as.de>
2020-11-27 21:25:57 +0100

    modal form

--- care4as/resources/views/home.blade.php
+++ care4as/resources/views/home.blade.php
@@ -1,8 +1,28 @@
 @extends('general_layout')
 
 @section('content')
-
-test
-
-
+<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="row" style="background-color:rgba(114,227,242,1); border-bottom: 5px solid black;">
+        <div class="col">
+          <h5>titlevariabel</h5>
+        </div>
+        <div class="d-flex float-right align-self-center">
+          <img src="" class="img-fluid p-3" alt="firmenlogo">
+        </div>
+      </div>
+      <div class="row" style="background-color: rgba(72,72,72,1); border-bottom: 5px solid black;">
+        <div class="col-12">
+          <p>content</p>
+        </div>
+      </div>
+      <div class="row bg-light">
+        <div class="col text-dark">
+          <p>footer</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
