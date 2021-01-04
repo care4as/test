7dafc8a31fe7481d827c4c554680cb6f05bfae6e
AndreasRobrahn <andreasrobrahn@care4as.de>
2021-01-04 13:47:41 +0100

    Netze für Angebote eingestellt

--- care4as/app/Offer.php
+++ care4as/app/Offer.php
@@ -6,12 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offer extends Model
 {
-    public function saveOffer($name,$volume,$price,$telefon,$categories=null)
+    public function saveOffer($name,$volume,$price,$telefon,$net,$categories=null)
     {
       $this->name = $name;
       $this->volume = $volume;
       $this->price = $price;
       $this->telefon = $telefon;
+      $this->mobile_net = $net;
 
       foreach ($categories as $value) {
         $this->$value = 1;
--- care4as/public/js/app.js
+++ care4as/public/js/app.js
@@ -2317,8 +2317,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
