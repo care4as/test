a43ac1848ffcf6736894f476394b3a6389e8a2f6
AndreasRobrahn <andimensional@gmail.com>
2021-08-04 11:20:46 +0200

    fix

--- care4as/app/Http/Controllers/HardwareController.php
+++ care4as/app/Http/Controllers/HardwareController.php
@@ -16,7 +16,7 @@ class HardwareController extends Controller
       $hardwareMO = Hardwareitem::where('type_id',2)->with('deviceMO')->get();
       $hardware = $hardwarePCs->merge($hardwareMO);
 
-      $hardware = (new Collection($hardware))->paginate(2);
+      $hardware = (new Collection($hardware))->paginate(20);
       // dd($hardware);
       return view('inventory.inventoryIndex', compact('hardware'));
     }
