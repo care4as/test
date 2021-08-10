28f292ec6c236a65fb334d17eb59daef937a4d81
AndreasRobrahn <andreasrobrahn@care4as.de>
2021-08-10 18:10:59 +0200

    excel export hinzugefügt

--- care4as/resources/views/inventory/inventoryIndex.blade.php
+++ care4as/resources/views/inventory/inventoryIndex.blade.php
@@ -60,15 +60,15 @@
           <div class="">Ort</div>
           <hr>
             <div class="">
-                <div style="display: grid; grid-template-columns: auto 1fr; column-gap: 10px;">
-                    <label for="inputState" style="margin: auto;">Auswahl:</label>
-                    <select id="inputState" class="form-control" name="place">
-                        <option selected>Alle</option>
-                        @foreach(DB::table('hardware_inventory')->pluck('place')->unique() as $place)
-                        <option value="{{$place}}">{{$place}}</option>
-                        @endforeach
-                    </select>
-                </div>
+              <div style="display: grid; grid-template-columns: auto 1fr; column-gap: 10px;">
+                <label for="inputState" style="margin: auto;">Auswahl:</label>
+                <select id="inputState" class="form-control" name="place">
+                    <option selected>Alle</option>
+                    @foreach(DB::table('hardware_inventory')->pluck('place')->unique() as $place)
+                    <option value="{{$place}}">{{$place}}</option>
+                    @endforeach
+                </select>
+              </div>
             </div>
           </div>
       </div>
@@ -80,7 +80,7 @@
     <div class="row">
       <div class="col p-0 m-2">
         <div class="table-responsive">
-          <table class="table table-borderless w-100 text-white table-spacing" id="hwtable">
+          <table class="table table-borderless w-100 text-white tablespacing" id="hwtable">
             <thead>
             <tr class="unit-translucent">
               <th>id</th>
@@ -113,7 +113,6 @@
       </div>
     </div>
   </div>
-
 @endsection
 
 @section('additional_modal')
@@ -313,14 +312,27 @@
 <script src='https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js'></script>
 <script src='https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js'></script>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" type="text/javascript"></script>
+
 <script type="text/javascript">
-let table = $('#hwtable').DataTable()
+let table = $('#hwtable').DataTable({
+  dom: 'Blfrtip',
+  buttons: [
+          { extend: 'csv', text: '<i class="fas fa-file-csv fa-2x"></i>' },
+          { extend: 'excel', text: '<i class="fas fa-file-excel fa-2x"></i>' },
+          // 'excel',
+      ]
+})
 
 $('#devicetype').on('change', function() {
 
   let devicetype = this.value
 
-  console.log(devicetype)
+  // console.log(devicetype)
 
   switch(devicetype) {
     case '1':
